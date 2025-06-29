c39a6c216d9c8cdbb5ab0d7afde8da7bfee67867
William Jacobsen <villi05.v.j@gmail.com>
2025-06-29 16:56:39 +0200

    basic frontend and backend setup
    basic vite+react+typescript+tailwindcss+lucide frontend prompt interface.
    basic backend java spring boot.

---
+++ WebApp/backend/closedai/src/main/java/com/closedai/closedai/ClosedaiApplication.java
@@ -0,0 +1,13 @@
+package com.closedai.closedai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosedaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosedaiApplication.class, args);
+	}
+
+}
---
+++ WebApp/backend/closedai/src/main/java/com/closedai/closedai/home/homeController.java
@@ -0,0 +1,17 @@
+package com.closedai.closedai.home;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class homeController {
+    
+    @GetMapping
+    public String home()
+    {
+        return "test";
+    }
+
+}
