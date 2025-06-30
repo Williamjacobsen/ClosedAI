2b907725168a9b22ed2e196eab6798666eeca840
William Jacobsen <villi05.v.j@gmail.com>
2025-06-30 23:42:21 +0200

    not finished but progress to handle tables

--- ClosedAI/main.py
+++ ClosedAI/main.py
@@ -60,6 +60,56 @@ def get_response(scraper: Scraper):
 
     response = wait_for_response(scraper, "/html/body/div[1]/div/div[1]/div[2]/main/div/div/div[1]/div/div/div[2]/article[last()]/div/div/div/div/div[1]")
 
+    formatted_response = ""
+
+    # /html/body/div[1]/div/div[1]/div[2]/main/div/div/div[1]/div/div/div[2]/article[last()]/div/div/div/div/div[1]/div/div/div
+    response_elements = scraper.GetChildren("/html/body/div[1]/div/div[1]/div[2]/main/div/div/div[1]/div/div/div[2]/article[last()]/div/div/div/div/div[1]/div/div/div")
+
+    # /html/body/div[1]/div/div[1]/div[2]/main/div/div/div[1]/div/div/div[2]/article[last()]/div/div/div/div/div[1]/div/div/div/p[1] # text
+    # /html/body/div[1]/div/div[1]/div[2]/main/div/div/div[1]/div/div/div[2]/article[last()]/div/div/div/div/div[1]/div/div/div/div # table (class="_tableContainer_80l1q_1")
+    # /html/body/div[1]/div/div[1]/div[2]/main/div/div/div[1]/div/div/div[2]/article[last()]/div/div/div/div/div[1]/div/div/div/p[2] # text
+
+    for element in response_elements:
+        print(element.tag_name) # p for text and div for tables
+        print(element.get_attribute("class")) # _tableContainer_80l1q_1 for tables
+
+        tag = element.tag_name
+        _class = element.get_attribute("class")
+
+        if tag == "p": # text
+            formatted_response += scraper.GetTextFromElement(element)
+        
+        elif tag == "div" and _class == "_tableContainer_80l1q_1": # table
+
+            # Structure:
+            # div: _tableContainer_80l1q_1
+            #   div
+            #       table
+            #           thead
+            #               tr - (first) row of keys
+            #                   th - each column / value of the row
+            #                   ...
+            #                   th
+            #           tbody
+            #               tr - each row (of values)
+            #               ...
+            #               tr
+            #                   td - each value of the row
+            #                   ...
+            #                   td
+
+            thead = scraper.FindFirstTagFromElement(element, "thead")
+            tr = scraper.GetChildByIndex(thead, 0)
+            th_list = scraper.GetChildrenFromElement(tr)
+            for th in th_list:
+                print(scraper.GetTextFromElement(th)) # TODO: handle this
+            # TODO
+
+            formatted_response += scraper.GetTextFromElement(element)
+        
+        else:
+            formatted_response += scraper.GetTextFromElement(element)
+
     return response
 
 if __name__ == '__main__':
