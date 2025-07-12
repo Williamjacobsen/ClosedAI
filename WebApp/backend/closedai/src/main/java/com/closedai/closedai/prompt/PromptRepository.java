789be6e7ede61f740c779c95d3233c06fb99f2ec
William Jacobsen <villi05.v.j@gmail.com>
2025-07-12 18:34:16 +0200

    backend clean up

--- WebApp/backend/closedai/src/main/java/com/closedai/closedai/prompt/PromptRepository.java
+++
@@ -1,8 +0,0 @@
-package com.closedai.closedai.prompt;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PromptRepository extends CrudRepository<Prompt, String> {
-}
