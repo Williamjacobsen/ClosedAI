07932cca8ffe2376795a33380435267ae48a130c
William Jacobsen <villi05.v.j@gmail.com>
2025-07-16 19:57:03 +0200

    added cors to backend

---
+++ WebApp/backend2/closedai/src/main/java/com/closedai/closedai/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.closedai.closedai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
