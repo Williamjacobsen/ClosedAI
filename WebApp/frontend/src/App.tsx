22010f18ba2d5bd33cc2f1bbea4e234ed25b25ce
Williamjacobsen <villi05.v.j@gmail.com>
2025-08-22 19:11:32 +0200

    added sidebar

--- WebApp/frontend/src/App.tsx
+++ WebApp/frontend/src/App.tsx
@@ -45,64 +45,63 @@ function App() {
   // TODO: Should show user if an important error has happend
 
   return (
-    <div className="h-screen">
-      {/* <BallTriangle fill="#000000" width={50} height={50} /> */}
-      <div className="bg-gray-100">
-        <div className="flex flex-col h-screen">
-          {/* Title */}
-          <h1 className="text-4xl text-gray-800 py-10 text-center">
-            ClosedAI - Model C4
-          </h1>
+    <div className="h-screen flex">
+      {/* Sidebar */}
+      <div className="bg-black w-64 h-full shrink-0"></div>
 
-          {/* Chat History */}
-          <div className="flex-4 flex justify-center overflow-y-auto">
-            <div className="bg-white flex justify-center w-6xl h-full rounded-xl overflow-y-auto whitespace-pre-wrap">
-              <ul className="w-6xl px-10">
-                {messages.map((message, i) => (
-                  <li
-                    key={i}
-                    className={`my-2 flex ${
-                      message.type === "PROMPT"
-                        ? "justify-end"
-                        : "justify-start"
+      {/* Main */}
+      <div className="bg-gray-100 flex-1 flex-col">
+        {/* Title */}
+        <h1 className="text-4xl text-gray-800 py-10 text-center">
+          ClosedAI - Model C4
+        </h1>
+
+        {/* Chat History */}
+        <div className="flex-4 flex justify-center overflow-y-auto">
+          <div className="bg-white flex justify-center w-6xl h-full rounded-xl overflow-y-auto whitespace-pre-wrap">
+            <ul className="w-6xl px-10">
+              {messages.map((message, i) => (
+                <li
+                  key={i}
+                  className={`my-2 flex ${
+                    message.type === "PROMPT" ? "justify-end" : "justify-start"
+                  }`}
+                >
+                  <div
+                    className={`p-3 rounded-lg max-w-4xl overflow-x-auto ${
+                      message.type === "RESPONSE"
+                        ? "bg-gray-300 text-black rounded-bl-none"
+                        : "bg-gray-100 text-black rounded-br-none"
                     }`}
                   >
-                    <div
-                      className={`p-3 rounded-lg max-w-4xl overflow-x-auto ${
-                        message.type === "RESPONSE"
-                          ? "bg-gray-300 text-black rounded-bl-none"
-                          : "bg-gray-100 text-black rounded-br-none"
-                      }`}
-                    >
-                      {message.message}
-                    </div>
-                  </li>
-                ))}
-                {sendingRef.current ? (
-                  <div className="bg-gray-100 text-black rounded-bl-none p-3 rounded-lg w-25">
-                    <h4 className="font-bold text-lg">Loading:</h4>
-                    <BallTriangle fill="#000000" width={80} height={80} />
+                    {message.message}
                   </div>
-                ) : null}
-                <li className="w-1 h-5"></li>
+                </li>
+              ))}
+              {sendingRef.current ? (
+                <div className="bg-gray-100 text-black rounded-bl-none p-3 rounded-lg w-25">
+                  <h4 className="font-bold text-lg">Loading:</h4>
+                  <BallTriangle fill="#000000" width={80} height={80} />
+                </div>
+              ) : null}
+              <li className="w-1 h-5"></li>
 
-                <div ref={divScrollDownRef} />
-              </ul>
-            </div>
+              <div ref={divScrollDownRef} />
+            </ul>
           </div>
+        </div>
 
-          {/* Input field */}
-          <div className="flex-1 flex justify-center">
-            <div className="h-full w-2xl flex justify-center items-center">
-              <textarea
-                onChange={(e) => setPrompt(e.target.value)}
-                onKeyDown={handleKeyDown}
-                value={prompt}
-                required
-                placeholder="Prompt..."
-                className="bg-white h-4/5 w-2xl rounded-md p-2 resize-none text-lg"
-              />
-            </div>
+        {/* Input field */}
+        <div className="flex-1 flex justify-center">
+          <div className="h-full w-2xl flex justify-center items-center">
+            <textarea
+              onChange={(e) => setPrompt(e.target.value)}
+              onKeyDown={handleKeyDown}
+              value={prompt}
+              required
+              placeholder="Prompt..."
+              className="bg-white h-4/5 w-2xl rounded-md p-2 resize-none text-lg"
+            />
           </div>
         </div>
       </div>
