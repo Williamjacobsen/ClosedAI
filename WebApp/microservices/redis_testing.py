d61fe465860616cac3b515bb51a9b30d684f66f4
Williamjacobsen <villi05.v.j@gmail.com>
2025-08-24 14:27:36 +0200

    small bug fix

--- WebApp/microservices/redis_testing.py
+++ WebApp/microservices/redis_testing.py
@@ -3,18 +3,21 @@ import time
 
 client = RedisManager()
 
+
 def pub_prompt():
     client.redis_publisher("prompt_channel", {"key": "testid", "value": "testprompt"})
 
+
 def pub_response(data):
     client.redis_publisher(
-        "response_channel", 
+        "response_channel",
         {
-            "sessionId": data.sessionId,
-            "chatSessionName": data.chatSessionName,
-            "response": "my test response"
-        }
+            "sessionId": data["sessionId"],
+            "chatSessionName": data["chatSessionName"],
+            "response": "my test response",
+        },
     )
+
 
 def callbackFunc(channel, data):
     print(f"Message on [{channel}]:")
@@ -22,12 +25,17 @@ def callbackFunc(channel, data):
     if channel == "prompt_channel":
         simulate_bot_response(data)
 
+
 def subscribers():
-    client.redis_subscriber(channels=["prompt_channel", "response_channel"], callback=callbackFunc)
+    client.redis_subscriber(
+        channels=["prompt_channel", "response_channel"], callback=callbackFunc
+    )
+
 
 def simulate_bot_response(data):
     time.sleep(5)
     pub_response(data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     subscribers()
